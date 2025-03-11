fe4e285c65063d9b258a151f47d6aa42d3d4106b
kvnbanunu <kvnbanunu@gmail.com>
2025-03-11 10:36:50 -0700

    corsoptions

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,19 @@ const cors = require('cors');
 
 const app = express();
 
-let corsOptions = {
-    origin : ['https://kvnbanunu.github.io'],
-}
+const corsOptions = {
+    origin: 'https://kvnbanunu.github.io',
+    methods: 'GET,POST,PUT,DELETE', // Allowed HTTP methods
+    allowedHeaders: 'Content-Type,Authorization', // Allowed headers
+    credentials: true // If you need to allow cookies or authentication headers
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+
+app.options('*', cors(corsOptions));
 
 app.get('/', (req, res) => {
     res.json('express test');
@@ -24,7 +31,7 @@ app.post('/json', (req, res) => {
     res.json(body);
 });
 
-app.get('/cors', cors(corsOptions), (req, res) => {
+app.get('/cors', (req, res) => {
     res.json('cors test');
 });
 
