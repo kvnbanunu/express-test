83ee7b3fd8a77e157760596c616570ce71eaaae2
kvnbanunu <kvnbanunu@gmail.com>
2025-03-11 10:48:34 -0700

    nocors

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,20 @@
 const express = require('express');
-const cors = require('cors');
 
 const app = express();
 
-const corsOptions = {
-    origin: 'https://kvnbanunu.github.io',
-    methods: 'GET,POST,PUT,DELETE', // Allowed HTTP methods
-    allowedHeaders: 'Content-Type,Authorization', // Allowed headers
-    credentials: true // If you need to allow cookies or authentication headers
-};
+app.use(function(){
+    res.setHeader('Access-Control-Allow-Origin', 'https://kvnbanunu.github.io');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
 
-app.use(cors(corsOptions));
+    // Handle preflight requests
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200); // No Content
+    }
+
+    next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
